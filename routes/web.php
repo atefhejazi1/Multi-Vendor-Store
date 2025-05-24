45acdd52999d33719f944e7d32be4d019bc473ea
Atef Hejazi <atefhejazi10@gmai.com>
2025-05-24 15:33:11 +0300

    making add to cart with all functionalities

--- routes/web.php
+++ routes/web.php
@@ -1,9 +1,20 @@
 <?php
 
+use App\Http\Controllers\front\CartController;
 use App\Http\Controllers\front\HomeController;
+use App\Http\Controllers\front\ProductsController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [HomeController::class, 'index'])->name('home');
+
+
+Route::get('/products', [ProductsController::class, 'index'])
+    ->name('products.index');
+
+Route::get('/products/{product:slug}', [ProductsController::class, 'show'])
+    ->name('products.show');
+
+Route::resource('/cart', CartController::class);
 
 
 require __DIR__ . '/auth.php';
